64021e3fe9ff3bd20117475e08ff82e39abdfd4d
wenbo.yang <wenbo.yang.84@gmail.com>
2024-08-01 09:51:56 -0700

    merge

---
+++ test/unit/lc0326.spec.ts
@@ -0,0 +1,15 @@
+xdescribe('leetcode 326: power of 3', () => {
+    function isPowerOfThree(n: number): boolean {
+        if (n <=0) {
+            return false;
+        }
+
+        while (n % 3 === 0 && n >= 3) {
+            n = n / 3;
+        }
+
+        return n === 1
+    };
+
+    it('test case 1 Input:, target = 5, output 2 ', () => {});
+});
---
+++ test/unit/lc0327.spec.ts
@@ -0,0 +1,7 @@
+xdescribe('leetcode 327: count of range sum', () => {
+    function countRangeSum(nums: number[], lower: number, upper: number): number {
+        
+    };
+
+    it('test case 1 Input:, target = 5, output 2 ', () => {});
+});
